eff62deba9a2c1f15e074adfdb283d8cd21072c2
kikorobledo <enrique_j_@hotmail.com>
2024-05-31 14:06:37 -0600

    Fix avaluo

--- resources/views/avaluos/avaluo.blade.php
+++ resources/views/avaluos/avaluo.blade.php
@@ -731,7 +731,7 @@
                                             <p>{{ collect($predio->condominioTerrenos)->sum('superficie_proporcional') }}</p>
                                         </td>
                                         <td style="padding-right: 40px; font-size:10px; border: 1px solid black; text-align: right;">
-                                            <p>{{ $predio->superficie_terreno  + collect($terrenosCondominio)->sum('superficie_proporcional') }}</p>
+                                            <p>{{ $predio->superficie_terreno  + collect($condominioTerrenos)->sum('superficie_proporcional') }}</p>
                                         </td>
                                     </tr>
 
@@ -746,7 +746,7 @@
                                             <p>{{ collect($predio->condominioConstrucciones)->sum('superficie_proporcional') }}</p>
                                         </td>
                                         <td style="padding-right: 40px; font-size:10px; border: 1px solid black; text-align: right;">
-                                            <p>{{ $predio->superficie_construccion  + collect($construccionesCondominio)->sum('superficie_proporcional') }}</p>
+                                            <p>{{ $predio->superficie_construccion  + collect($condominioConstrucciones)->sum('superficie_proporcional') }}</p>
                                         </td>
                                     </tr>
 
